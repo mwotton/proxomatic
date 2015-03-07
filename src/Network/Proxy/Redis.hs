d2e33bffc3f5f17de3853df0d948924a9604467f
Mark Wotton <mwotton@gmail.com>
2015-03-07 17:35:38 +0700

    add redis parser

--- src/Network/Proxy/Redis.hs
+++ src/Network/Proxy/Redis.hs
@@ -1,5 +1,14 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Network.Proxy.Redis where
+import           ClassyPrelude
+import           Control.Applicative              ((<$>))
+import           Data.Attoparsec.ByteString.Char8 (anyChar, manyTill, string)
+import qualified Data.ByteString.Char8            as BS
 import           Network.Proxy.Types
+import           Prelude                          ()
 
 proxy :: [(Host, Port)] -> Proxy
-proxy = undefined
+proxy ports = Proxy parser (return True)
+
+parser = (<>"\r\n") . BS.pack <$>
+         manyTill anyChar (string "\r\n") <* string "\r\n"
