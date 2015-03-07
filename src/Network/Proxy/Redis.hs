cac4b42bfd4c62b271065a798c7cf916c2a457ee
Mark Wotton <mwotton@gmail.com>
2015-03-07 17:38:45 +0700

    tiny cleanup

--- src/Network/Proxy/Redis.hs
+++ src/Network/Proxy/Redis.hs
@@ -1,7 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Network.Proxy.Redis where
 import           ClassyPrelude
-import           Control.Applicative              ((<$>))
 import           Data.Attoparsec.ByteString.Char8 (anyChar, manyTill, string)
 import qualified Data.ByteString.Char8            as BS
 import           Network.Proxy.Types
